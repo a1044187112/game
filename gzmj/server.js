c2fbc5500c6b18f2e7b461cf44a5c4fb7bc33868
a1044187112 <1044187112@qq.com>
2018-06-21 10:04:52 +0800

    Signed-off-by: a1044187112 <1044187112@qq.com>

--- gzmj/server.js
+++ gzmj/server.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var router = require("router");
+
 var app = express();
 // 添加json解析
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
+var router = express.Router();
 
 app.get('/', function(req, res) {
     res.send('凯恩!');
 });
 
+router.post('/login', function(req, res, next) {
+
+  // 获取参数
+  var query = req.body;
+  console.log("post请求：参数", query);
+
+  res.send('hello , world');
+});
 
 app.listen(3000);
