f5dd663f41a83297a185536e380c478c2c664c29
seed3803421 <seed3803421@126.com>
2018-06-25 18:10:30 +0800

    后台对接测试

--- gzmj/Cocos2DGame/assets/Script/LoginButton.js
+++ gzmj/Cocos2DGame/assets/Script/LoginButton.js
@@ -14,15 +14,27 @@ cc.Class({
             
             console.log("登录");
 
+            // 登录接口IP
             let url = "http://192.168.1.162:3000/login";
             
+            // 要传送的数据
             // let data = JSON.stringify({"name":"test"});
-
             let data = "name=test&age=16&id=05";
             
-            let test = this.http("POST",url,data);
+            // 解析传送过来的 JSON 数据
+            let test = JSON.parse(this.http("POST",url,data));
+            
+            console.log(test);
 
-            console.log(JSON.stringify(test));
+            if (test.state = "success") {
+                
+                cc.director.loadScene("Hall");
+
+            }else{
+
+                console.log("登录失败");
+
+            }
 
         },this);
 
@@ -36,22 +48,27 @@ cc.Class({
     // 后台请求数据函数
     http: function(type,url,data){
 
+        // 创建一个HTTP
         let xmlhttp = new XMLHttpRequest();
         
+        // 打开HTTP
         xmlhttp.open(type,url,false);
         
         xmlhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
         
         if(type == "GET"){
 
+            // 传送的数据
             xmlhttp.send(null);
 
         }else if(type == "POST"){
 
+            // 传送的数据
             xmlhttp.send(data);
 
         };
 
+        // 获取传输的数据
         return xmlhttp.responseText;
     },
 });
