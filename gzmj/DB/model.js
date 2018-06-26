3d51e8854f5991262c76d5e2495ef10d919ca5ca
seed3803421 <seed3803421@126.com>
2018-06-26 10:40:34 +0800

    数据库模板增加

--- gzmj/DB/model.js
+++ gzmj/DB/model.js
@@ -25,14 +25,14 @@ function CreateTable(sql){
 // 用户表（ID,游戏ID,用户名，房卡，创建时间，登录时间）
 var gameuser =  'create table gameuser(' +
                 'id int primary key auto_increment,'+
-                'GameID int not null,'+
+                'GameID mediumtext not null,'+
                 'User varchar(50) not null,'+
                 'RoomCard int not null,'+
                 'CreateTime TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP,'+
                 'LoginTime TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP'+
             ')';
 
-CreateTable(gameuser);
+// CreateTable(gameuser);
 
 // 公告表（ID，公告名称，公告内容，备注，创建时间）
 var announcement = 'create table announcement(' +
@@ -43,7 +43,28 @@ var announcement = 'create table announcement(' +
                    'Time TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP'+
                 ')';
 
-CreateTable(announcement);
+// CreateTable(announcement);
+
+//房间信息表(房间ID、创建房间的用户ID、游戏类型、房间信息、游戏玩家)
+var RoomInformation = 'create table RoomInformation(' +
+                      'RoomID varchar(6) primary key,' + 
+                      'UserID mediumtext not null,' +
+                      'Gametype int not null,' +
+                      'RoomInfo json not null,' +
+                      'GamePlayer varchar(100) not null' +
+                      'Time TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP' +
+                    ')';
+
+CreateTable(RoomInformation);
+
+// 战绩表
+var Record = 'create table Record(' +
+             'id int primary key auto_increment,'+
+             'UserID mediumtext not null,' +
+             'RecordInfo json not null,' +
+            ')';
+
+CreateTable(Record);
 
 // 关闭连接
 db.end();
